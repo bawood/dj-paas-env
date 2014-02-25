d84265f7494c066117ac0d26191ab3f0f7826e3d
Germán Moya <pbacterio@gmail.com>
2014-02-25 15:16:59 +0100

    packaging

--- setup.py
+++ setup.py
@@ -2,19 +2,26 @@
 from setuptools import setup
 from sys import version_info
 
-if version_info < (2, 7):
-    test_require = ['unittest2']
-else:
-    test_require = []
-
 setup(
     name='dj-paas-env',
-    version='',
+    description='Helper methods to configure Django database and static files in a PAAS environment.',
+    version='0.1b1',
     packages=['dj_paas_env'],
-    url='',
-    license='',
+    url='https://github.com/pbacterio/dj-paas-env',
+    license='MIT',
     author='Germán Moya',
-    author_email='',
+    author_email='pbacterio@gmail.com',
     test_suite='dj_paas_env.test.suite',
-    test_require=test_require
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
